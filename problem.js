466559c5f963e34f4db3f5b51990d2b3381566ca
Abir <ak.cr852456@gmail.com>
2021-12-19 03:18:27 +0600

    first javascript

---
+++ problem.js
@@ -0,0 +1,11 @@
+var abir_age = 18;
+var aimanvai_age = 30;
+
+var addition = abir_age + aimanvai_age;
+
+if (abir_age > aimanvai_age){
+ console.log("Abir is Younger")
+} else{
+ console.log("Aiman Vaiya onk boro")
+}
+
